00abaa4180b2da844ca608d443d950c3ac9dcbce
ignatius yesigye <ignatiusyesigye@gmail.com>
2021-05-31 17:07:02 +0300

    Update users_view.php

--- application/modules/users/views/admin/users_view.php
+++ application/modules/users/views/admin/users_view.php
@@ -190,3 +190,177 @@ document.addEventListener("DOMContentLoaded", function() {
 		'<script type="text/javascript" src="'.base_url('assets/js/app.datatables.js').'"></script>'
 	]
 ]) ?>
+
+
+
+@extends('layouts.contentLayoutMaster')
+{{-- page title --}}
+@section('title','Destination Banks')
+{{-- vendor styles --}}
+@section('vendor-styles')
+<link rel="stylesheet" type="text/css" href="{{asset('vendors/css/tables/datatable/dataTables.bootstrap4.min.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('vendors/css/tables/datatable/responsive.bootstrap4.min.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('vendors/css/tables/datatable/buttons.bootstrap4.min.css')}}">
+@endsection
+{{-- page styles --}}
+@section('page-styles')
+<link rel="stylesheet" type="text/css" href="{{asset('css/pages/app-users.css')}}">
+@endsection
+@section('content')
+<!-- banks list start -->
+<section class="banks-list-wrapper">
+    <h4>Destination Banks</h4>
+    <hr>
+    <div class="banks-list-table">
+        <div class="card">
+        <div class="card-body">
+            <!-- datatable start -->
+            <div class="table-responsive">
+                <table id="banks-list-datatable" class="table">
+                    <thead>
+                    <tr>
+                        <th>Bank Name</th>
+                        <th>Destination</th>
+                        <th class="text-center">Action</th>
+                    </tr>
+                    </thead>
+                    <tbody></tbody>
+                </table>
+            </div>
+            <!-- datatable ends -->
+        </div>
+        </div>
+    </div>
+</section>
+<!-- banks list ends -->
+@foreach ($banks as $bank)
+{{-- Edit user roles modal --}}
+<div class="modal fade" id="edit-modal{{ $bank->id }}" tabindex="-1" aria-labelledby="edit-modalTitle{{ $bank->id }}" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="edit-modalTitle{{ $bank->id }}">Edit Destination Bank</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <i class="bx bx-x"></i>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form method="POST" action="{{ url('config/banks') }}">
+          @csrf
+          @method('put')
+          <input type="hidden" name="id" value="{{ $bank->id }}">
+          <div class="form-group">
+            <label for="destination">Destination</label>
+            <select class="form-control" name="destination" id="field-edit-destination">
+              @foreach ($destinations as $destination)
+                <option value="{{ $destination->id }}">{{ $destination->country->name }}</option>
+              @endforeach
+            </select>
+          </div>
+          <div class="form-group">
+            <label for="name">Bank name</label>
+            <input
+            type="text"
+            name="bank"
+            id="field-edit-bank"
+            class="form-control"
+            placeholder="Enter bank name"
+            value="{{$bank->name}}">
+          </div>
+          <button type="submit" class="btn btn-primary">
+            <i class="bx bx-x d-block d-sm-none"></i>
+            <span class="d-none d-sm-block">Save</span>
+          </button>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+@endforeach
+@endsection
+
+{{-- vendor scripts --}}
+@section('vendor-scripts')
+<script src="{{asset('vendors/js/tables/datatable/jquery.dataTables.min.js')}}"></script>
+<script src="{{asset('vendors/js/tables/datatable/dataTables.bootstrap4.min.js')}}"></script>
+<script src="{{asset('vendors/js/tables/datatable/dataTables.buttons.min.js')}}"></script>
+<script src="{{asset('vendors/js/tables/datatable/buttons.bootstrap4.min.js')}}"></script>
+<script src="{{asset('vendors/js/extensions/moment.min.js')}}"></script>
+@endsection
+
+{{-- page scripts --}}
+@section('page-scripts')
+    <script src="{{asset('assets/js/global.js')}}"></script>
+    <script src="{{asset('js/scripts/pages/app-users.js')}}"></script>
+    @error('description')
+        <script>$('#create-modal').modal("show")</script>
+    @enderror
+    <script>
+
+        function editBank(data) {
+            console.log(data)
+        }
+    document.addEventListener("DOMContentLoaded", function() {
+
+        // Define new create item modal
+        var create_modal = $('#user-create-modal');
+        
+        // Define datatable
+        const table = $('#banks-list-datatable').DataTable({
+            "rowId": 'id',
+            data: JSON.parse(`<?= $banks->toJson() ?>`),
+            columns: [
+                { data: "name" },
+                {
+                    data: "destination",
+                    className: "text-center",
+                    render: function (destination, type, row) {
+                        return destination.country.name
+                    }
+                }
+            ],
+            columnDefs: [{
+                targets: 2,
+                className: "text-center",
+                render: function(data, type, row, meta) {
+                    let template = '';
+
+                    if(type === 'display') {
+                        template = `
+                            <a href="#" onclick="editBank(this)">
+                                <i class="bx bx-edit-alt"></i>
+                            </a>
+                        `;
+                    }
+
+                    return template;
+                }
+            }],
+            select: {
+                style: 'multi'
+            },
+            order: [[1, 'asc']],
+            "drawCallback": function () {
+                $('.dataTables_paginate > .pagination').addClass('pagination-sm');
+            }
+        });
+
+        // Delete item from datatable
+        $('#table').on('click', '.delete-row', function() {
+            var row = $(this).parents('tr');
+            // AJAX to delete item
+            $.ajax({
+                url: '' + row.attr('id'),
+                type: 'DELETE',
+                success: function(result) {
+                    table.row(row).deselect().remove().draw();
+                },
+            });
+        });
+
+        create_modal.on('shown.bs.modal', function (e) {
+            $(this).first('input').focus();
+        })
+    });
+    </script>
+@endsection
